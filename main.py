6c477d597ab37048ab8469b02630d99fbe0d7a08
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-06-10 15:49:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from argparse import ArgumentParser
+from sys import exit as exiter
 
+from pyrogram import Client
+
+pbot = Client("pyrocil", 0, "", bot_token="", no_updates=True)
+parser = ArgumentParser(description="Pyro Downloader !")
+parser.add_argument("file_id", type=str, help="File id of the file.")
+parser.add_argument("name", type=str, help="File name to be given to the downloaded media.")
+
+args = parser.parse_args()
+try:
+    pbot.start()
+except:
+    exiter(1)
+if file_id := args.get("file_id"):
+    try:
+        print(await pbot.download_media(file_id, file_name=args.get("name"), block=True))
+        pbot.stop()
+        exiter(0)
+    except Exception:
+        pass
+exiter(1)
